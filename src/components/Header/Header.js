d9493916b6d4116d4d4b497bdfa3e2cf92d77fff
Kareem ElZeky <kareem.elzeky@gmail.com>
2022-03-20 17:56:12 +0200

    added initial folders and their boilerplate

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const componentName = () => {
+    return (
+        <header>
+            <div className= 'logo'></div>
+        </header>
+    );
+};
+
+export default componentName;
---
+++ src/features/PostDetails/PostDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'PostDetails.css';
+
+const componentName = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default componentName;
